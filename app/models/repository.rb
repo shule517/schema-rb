f7b08ed04569a6743a3b2707b06d7bcd3de1b207
shule <shule517@gmail.com>
2019-05-06 22:06:59 +0900

    テーブルを更新

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: repositories
+#
+#  id               :integer          not null, primary key
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  name             :string
+#  full_name        :string
+#  description      :string
+#  html_url         :string
+#  homepage         :string
+#  language         :string
+#  stargazers_count :integer
+#  pushed_at        :datetime
+#
+
 class Repository < ApplicationRecord
 end
--- db/ridgepole.rb
+++ db/ridgepole.rb
@@ -6,5 +6,13 @@ create_table "users", force: :cascade do |t|
 end
 
 create_table "repositories", force: :cascade do |t|
+  t.string 'name'
+  t.string 'full_name'
+  t.string 'description'
+  t.string 'html_url'
+  t.string 'homepage'
+  t.string 'language'
+  t.integer 'stargazers_count'
+  t.datetime 'pushed_at'
   t.timestamps
 end
