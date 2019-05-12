2f5cd1320a0d651f62672e2aa198c90dd2c075a6
shule <shule517@gmail.com>
2019-05-12 19:05:33 +0900

    関連テーブル名を表示する

--- app/controllers/simulators_controller.rb
+++ app/controllers/simulators_controller.rb
@@ -2,5 +2,6 @@ class SimulatorsController < ApplicationController
   def index
     @model_name = params['model_name'] || 'User'
     @table_name = @model_name.tableize
+    @tables = Table.where('name like ?', "%#{@model_name}%")
   end
 end
