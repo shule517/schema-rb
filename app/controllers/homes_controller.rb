c6b034753840ceaa2857079abb50f2b6192567d3
shule <shule517@gmail.com>
2019-05-12 18:39:05 +0900

    Routeを修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,4 @@
-class HomeController < ApplicationController
+class HomesController < ApplicationController
   def index
     @tables = Table.all
     @repositories = Repository.rails.order(stargazers_count: :desc)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get 'home/index'
-  get 'tables/index'
+  resources :tables
+  resources :homes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
